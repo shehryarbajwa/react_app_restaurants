0afef324ad7331b05bd2165ea68e6c6275186025
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-10-04 04:43:01 +0500

    UX updated

--- first_react_project/src/components/Business/Business.js
+++ first_react_project/src/components/Business/Business.js
@@ -9,7 +9,7 @@ class Business extends React.Component {
         return(
             <div className="Business">
                 <div className="image-container">
-                     <img src={business.imgSrc} alt=''/>
+                     <img src={business.imageSrc} alt=''/>
             </div>
                 <h2>{business.name}</h2>
             <div className="Business-information">
--- first_react_project/src/components/BusinessList/BusinessList.js
+++ first_react_project/src/components/BusinessList/BusinessList.js
@@ -8,7 +8,7 @@ class BusinessList extends React.Component {
         return (
             <div className="BusinessList">
             {
-                this.props.businesses.map ((business) => {
+                this.props.business.map ((business) => {
                     return <Business business={business} />;
                 })
             }
