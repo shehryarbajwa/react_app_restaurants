7f20f4fa52c409611aa8f99bfa86012ff532387a
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-10-04 04:41:18 +0500

    refactored code for components using prop

--- first_react_project/src/components/Business/Business.js
+++ first_react_project/src/components/Business/Business.js
@@ -2,36 +2,26 @@ import React from 'react'
 import './Business.css'
 
 
-const business = {
-    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-}
 
 class Business extends React.Component {
     render(){
+        const {business} = this.props ;
         return(
             <div className="Business">
                 <div className="image-container">
-                     <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+                     <img src={business.imgSrc} alt=''/>
             </div>
-                <h2>MarginOtto Pizzeria</h2>
+                <h2>{business.name}</h2>
             <div className="Business-information">
                 <div className="Business-address">
-                 <p>1010 Islington Avenue Way</p>
-                 <p>Bordertown</p>
-                 <p>NY 10101</p>
+                 <p>{business.address}</p>
+                 <p>{business.city}</p>
+                 <p>{`${business.state} ${business.zipCode}`}</p>
             </div>
             <div className="Business-reviews">
-                <h3>ITALIAN</h3>
-                <h3 className="rating">4.5 stars</h3>
-                <p>90 reviews</p>
+                <h3>{business.category.toUpperCase()}</h3>
+                <h3 className="rating">{`${business.rating} stars`}</h3>
+                <p>{`${business.reviewCount} reviews`}</p>
             </div>
         </div>
     </div>
